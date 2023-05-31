bb23e4500b292462f818dd94bc1a732edcd1b873
migue <miguelanjo2@hotmail.com>
2023-05-31 11:07:52 +0100

    added project list

---
+++ src/helpers/projectList.js
@@ -0,0 +1,13 @@
+import Proj1 from "../Images/proj1.jpg";
+
+
+export const ProjectList = [
+  {
+    name: "Study Buddy App",
+    image: Proj1,
+    skills: "<React.js><Node.js><PostgreSQL><BootStrapp><CSS>",
+    demo:"https://starter-kit-v0yo.onrender.com",
+    github: "https://github.com/Miguel-Cabral/web-warriors",
+    emaze: "https://www.emaze.com/@ALOZZWFCW/study-buddies?autoplay"
+  },
+ ]
