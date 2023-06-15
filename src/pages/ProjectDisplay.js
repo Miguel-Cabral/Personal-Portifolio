88b45b31609e0f022d4cebd0a08ffd024fd6936c
migue <miguelanjo2@hotmail.com>
2023-06-15 12:57:32 +0100

    updated and added buttons

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -7,13 +7,50 @@ import Button from "@material-ui/core/Button";
 
 
 const ProjectDisplay = () => {
- const { id } = useParams();
+  const { id } = useParams();
   const project = ProjectList[id];
   return (
     <div className='project'>
       <h1> {project.name}</h1>
       <img src={project.image} />
+
+      <p>
+        <b>Skills:</b> {project.skills}
+      </p>
+      <div className='buttons-container'>
+        <Button
+          variant="contained"
+          className='project-button'
+          style={{ backgroundColor: '#5a9a29', color: '#fff', marginRight: '10px' }}
+          href={project.demo}
+          target="_blank"
+
+        >
+          Live Demo
+        </Button>
+        <Button
+          variant="contained"
+          className='project-button'
+          style={{ backgroundColor: '#5a9a29', color: '#fff', marginRight: '10px' }}
+          href={project.github}
+          target="_blank"
+          startIcon={<GitHubIcon />}
+        >
+          Repository
+        </Button>
+        <Button
+          variant='contained'
+          className='project-button'
+          style={{ backgroundColor: '#5a9a29', color: '#fff' }}
+          href={project.emaze}
+          target='_blank'
+        >
+          Presentation
+        </Button>
+      </div>
+
     </div>
+
   )
 }
 
